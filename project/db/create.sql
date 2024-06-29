621046ac77dfa422feb7631f98092c99c47ff657
AgathaBahati <matinodev@gmail.com>
2024-06-29 21:43:09 +0300

    fix: create users table in users_dev
    - Modify create.sql to create users table in users_dev db

--- project/db/create.sql
+++ project/db/create.sql
@@ -2,3 +2,12 @@ CREATE DATABASE users_prod;
 CREATE DATABASE users_stage;
 CREATE DATABASE users_dev;
 CREATE DATABASE users_test;
+
+\c users_dev;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    confirmed BOOLEAN NOT NULL DEFAULT false,
+    email_sent BOOLEAN NOT NULL DEFAULT false
+);
