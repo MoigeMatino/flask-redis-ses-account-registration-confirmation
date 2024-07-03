1006f785eff1e9c7c0848b43fe370204d56b282d
AgathaBahati <matinodev@gmail.com>
2024-07-03 22:42:48 +0300

    feat: create send email task

---
+++ project/server/main/tasks.py
@@ -0,0 +1,13 @@
+import time
+
+from project.server import db
+from project.server.models import User
+
+
+def send_email(email):
+    # TODO: replace with actual sending email functionality
+    time.sleep(10) # simulate a process that takes time
+    user = User.query.filter(email == email).first()
+    user.email_sent = True
+    db.session.commit()
+    return True
