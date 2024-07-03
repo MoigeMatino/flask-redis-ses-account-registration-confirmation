e95117eaf3af713cea54d9237ba5ccab2d8f7a66
AgathaBahati <matinodev@gmail.com>
2024-07-03 23:03:32 +0300

    feat: add redis as message broker
    - Installed redis and rq
    
    - Update comose.yml with worker and redis services to handle queueing of tasks
    
    - Modify view to connect to redis and enqueue the task

--- project/server/main/tasks.py
+++ project/server/main/tasks.py
@@ -7,7 +7,7 @@ from project.server.models import User
 def send_email(email):
     # TODO: replace with actual sending email functionality
     time.sleep(10) # simulate a process that takes time
-    user = User.query.filter(email == email).first()
+    user = User.query.filter_by(email=email).first()
     user.email_sent = True
     db.session.commit()
     return True
--- project/server/main/views.py
+++ project/server/main/views.py
@@ -1,13 +1,15 @@
 # project/server/main/views.py
 
-
+import redis
 from flask import render_template, Blueprint, url_for, \
-    redirect, flash, request
+    redirect, flash, request, current_app
+from rq import Queue, Connection
 from sqlalchemy.exc import IntegrityError
 
 from project.server import db
 from project.server.models import User
 from project.server.main.forms import RegisterForm
+from project.server.main.tasks import send_email
 
 
 main_blueprint = Blueprint('main', __name__,)
@@ -22,6 +24,13 @@ def home():
                 user = User(email=form.email.data)
                 db.session.add(user)
                 db.session.commit()
+                redis_url = current_app.config['REDIS_URL']
+                # connect to redis
+                with Connection(redis.from_url(redis_url)):
+                    # create a new Queue instance
+                    q = Queue()
+                    # enqueue task to be executed
+                    q.enqueue(send_email, user.email)
                 flash('Thank you for registering.', 'success')
                 return redirect(url_for("main.home"))
             except IntegrityError:
