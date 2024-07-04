91f72a2c23cc65fd092a421397a770f60b2532e4
AgathaBahati <matinodev@gmail.com>
2024-07-04 23:04:08 +0300

    add confirm email view

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -10,7 +10,7 @@ from project.server import db
 from project.server.models import User
 from project.server.main.forms import RegisterForm
 from project.server.main.tasks import send_email
-from project.server.main.utils import encode_token, generate_url
+from project.server.main.utils import encode_token, generate_url, decode_token
 
 
 main_blueprint = Blueprint('main', __name__,)
@@ -45,3 +45,27 @@ def home():
                 flash('Sorry. That email already exists.', 'danger')
     users = User.query.all()
     return render_template('home.html', form=form, users=users)
+
+@main_blueprint.route('/confirm/<token>')
+def confirm_email(token):
+    decoded_email = decode_token(token)
+    if not decoded_email:
+        flash('The confirmation link is invalid.Please request a new one.', 'danger')
+        redirect(url_for('main.home'))
+    elif decoded_email == "Token expired":
+        flash('The confirmation link has expired. Please request a new one.', 'warning')
+        redirect(url_for('main.home'))
+    elif decoded_email == "Invalid token signature":
+        flash('The confirmation link is invalid. Please try again.', 'danger')
+    user = User.query.filter_by(email=decoded_email).first()
+    #ccheck if user is already confirmed
+    if user.confirmed:
+        flash('User already confirmed.', 'success')
+        return redirect(url_for('main.home'))
+    # confirm user and save status to db
+    user.confirmed = True
+    db.session.add(user)
+    db.commit()
+    flash('Your email has been successfully confirmed!', 'success')
+    return redirect(url_for('main.home'))
+
